39489d718f7d238fb1e0fd2df1e6d3d08eb1e775
aishasiddiqua-web <siddiquaaisha769@gmail.com>
2024-10-22 17:51:37 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+let accountNumber="12345";
+let exchangeNumber=Number(accountNumber);
+console.log(exchangeNumber);
+console.log(typeof exchangeNumber);
+let accountpassword=54321;
+let exchangepassword=String(accountpassword);
+console.log(exchangepassword);
+console.log(typeof exchangepassword);
---
+++ code2.js
@@ -0,0 +1,7 @@
+const accountId= "Aisha";
+let accountPassword="54321";
+var accountcity="karachi"
+let accountData="incomplete";
+var accountType="none";
+const accountEmail=""
+console.table({accountId, accountPassword, accountcity, accountData, accountType})
