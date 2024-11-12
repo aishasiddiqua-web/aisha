9bf4225c0cf3ea986ff3792dd7073d95562eb1a1
aishasiddiqua-web <siddiquaaisha769@gmail.com>
2024-11-12 17:53:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let myArray=[1,'Aisha',{text:'something'}]
+ console.log(Array.isArray(myArray))
+ console.log(myArray.length)
+ //myArray.splice(0,2)
+ //console.log(myArray);
+ myArray.push('orange')
+ console.log(myArray);
+ console.log(myArray[2]); 
+ let mixedBAg=['book',5,true,['nested']]
+ console.log(mixedBAg)
+ for (lt i =1;< mixedBAg.length; i++)
